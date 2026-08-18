acc86d01e545706c92c0a21961e8ba7d9ae49af2
zanfolim07 <guilhermezanfolimm@gmail.com>
2026-08-18 20:11:40 -0300

    added cart itens

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,17 +2,25 @@ import type { Product } from "../types/product"
 
 interface ProductCardProps {
     product: Product
+    onAddCart: (product: Product) => void
 }
 
-export function ProductCard({ product }: ProductCardProps) {
+export function ProductCard({ product, onAddCart }: ProductCardProps) {
 
     return (
-        <section>
+        <section style={{
+            display: 'flex',
+            width: "38%",
+            height: '10rem',
+            border: '2px black solid',
+            marginBottom: '0.5rem'
+        }}>
             <img src={product.image} alt={product.title} />
 
             <h3>{product.title}</h3>
             <p>{product.description}</p>
             <p>{product.price}</p>
+            <button onClick={() => onAddCart(product)}>Adicionar ao carrinho</button>
         </section>
     )
 }
